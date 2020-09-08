67a4b86a9ae2f72a50696cf361fc04d5ef75c579
bnlhv <benlahav1990@gmail.com>
2020-09-08 16:44:04 +0300

    Updates

---
+++ precache-manifest.21952ce09ee9f64aa27c93e694889e80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0570eb40fa0961c6fc4d90403bfc7728",
+    "url": "/bnlhv/RoboFriendsindex.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/bnlhv/RoboFriendsstatic/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3e9e0a6c2a98e8893f4d",
+    "url": "/bnlhv/RoboFriendsstatic/css/main.a19f36c8.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/bnlhv/RoboFriendsstatic/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bnlhv/RoboFriendsstatic/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e9e0a6c2a98e8893f4d",
+    "url": "/bnlhv/RoboFriendsstatic/js/main.56fc5aed.chunk.js"
+  },
+  {
+    "revision": "1202bf92fbb04a72bc79",
+    "url": "/bnlhv/RoboFriendsstatic/js/runtime-main.30914ae6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/bnlhv/RoboFriendsstatic/media/SEGA.12a3db8b.woff"
+  }
+]);
